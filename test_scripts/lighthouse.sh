2258785551cb2984a1512edeac428a220dabd5d5
Luther <luther@autostructure.io>
2020-01-31 18:17:43 -0500

    semicolons

--- test_scripts/lighthouse.sh
+++ test_scripts/lighthouse.sh
@@ -1,2 +1,3 @@
-npm run build
-npm run serve-static-files & npm run lighthouse & npm run stop-static-files
+npm run build;
+npm run serve-static-files & npm run lighthouse;
+npm run stop-static-files;
--- test_scripts/pa11y.sh
+++ test_scripts/pa11y.sh
@@ -1,2 +1,3 @@
-npm run build
-npm run serve-static-files & npm run pa11y & npm run stop-static-files
+npm run build;
+npm run serve-static-files & npm run pa11y;
+npm run stop-static-files;
